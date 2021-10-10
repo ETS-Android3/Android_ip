a7f96b5a8821c1be6aad5548ef1762cdb6594dc9
Mem Rop <judyrop97@gmail.com>
2021-10-11 02:35:10 +0300

    added onclickListener and butterKnife

---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.newsapp;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+   @BindView(R.id.exploreNewsButton) Button mExploreNewsButton;
+   @BindView(R.id.countryEditText) EditText mCountryEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mExploreNewsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String country = mCountryEditText.getText().toString();
+//                Log.d(TAG, country);
+                Intent intent = new Intent(MainActivity.this, NewsHighlightsActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Explore for More", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
