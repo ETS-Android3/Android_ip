a321d6131556b7a163e2c718c0d51ce121855ae2
Mem Rop <judyrop97@gmail.com>
2021-10-11 02:52:02 +0300

    created empty activity to check highlights

---
+++ app/src/main/java/com/example/newsapp/NewsActivity.java
@@ -0,0 +1,38 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class NewsActivity extends AppCompatActivity {
+@BindView(R.id.headlines) ListView mHeadlines;
+@BindView(R.id.moreNewsbutton) Button moreNewsButton;
+private String[] headlines = new String[] {"Corona Virus 19 Pandemic ,Vaccine and treatment", "16 die as plane crashes carrying parachutists crashes", "Austrian Chancellor quits after prosecutors raid his office","With some 'more open to conspiracy theories,'vaccination rates of English Premier League players remain shrouded in mystery"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,headlines);
+        mHeadlines.setAdapter(adapter);
+
+        moreNewsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent();
+//                startActivity(intent);
+                Toast.makeText(NewsActivity.this, "Wait for More in the next morning", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
