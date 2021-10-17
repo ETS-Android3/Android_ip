2c763d12aaca0a58da3093ca8ac5bab74424d315
Mem Rop <judyrop97@gmail.com>
2021-10-17 11:35:39 +0300

    created search file for responses api

--- app/src/main/java/com/example/newsapp/Constants.java
+++ app/src/main/java/com/example/newsapp/Constants.java
@@ -1,4 +1,6 @@
 package com.example.newsapp;
 
 public class Constants {
+    public static final String NEWS_BASE_URL  = "https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY";
+    public static final String API_KEY = BuildConfig.API_KEY;
 }
---
+++ app/src/main/java/com/example/newsapp/NewsSearchResponse.java
@@ -0,0 +1,117 @@
+
+package com.example.newsapp;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class NewsSearchResponse {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("category")
+    @Expose
+    private String category;
+    @SerializedName("language")
+    @Expose
+    private String language;
+    @SerializedName("country")
+    @Expose
+    private String country;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public NewsSearchResponse() {
+    }
+
+    /**
+     * 
+     * @param country
+     * @param name
+     * @param description
+     * @param language
+     * @param id
+     * @param category
+     * @param url
+     */
+    public NewsSearchResponse(String id, String name, String description, String url, String category, String language, String country) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.category = category;
+        this.language = language;
+        this.country = country;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
