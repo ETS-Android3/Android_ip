f60a003afdfafb4e4da7841bd2071b88c626fb92
Mem Rop <judyrop97@gmail.com>
2021-10-17 11:45:18 +0300

    added code to search news and query some strings

--- app/src/main/java/com/example/newsapp/NewsApi.java
+++ app/src/main/java/com/example/newsapp/NewsApi.java
@@ -1,4 +1,13 @@
 package com.example.newsapp;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface NewsApi {
+    @GET("news/search")
+    Call<NewsSearchResponse> getNews(
+            @Query("country") String country,
+            @Query("name") String name
+    );
 }
