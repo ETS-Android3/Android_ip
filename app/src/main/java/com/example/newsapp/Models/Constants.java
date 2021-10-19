9f1f5ab5d397e596fb3b043a2dad6cbcdd2b2e9a
Mem Rop <judyrop97@gmail.com>
2021-10-19 15:12:20 +0300

    made changes to base url

---
+++ app/src/main/java/com/example/newsapp/Models/Constants.java
@@ -0,0 +1,8 @@
+package com.example.newsapp.Models;
+
+import com.example.newsapp.BuildConfig;
+
+public class Constants {
+    public static final String NEWS_BASE_URL  = "https://newsapi.org/v2/top-headlines/";
+    public static final String API_KEY = BuildConfig.API_KEY;
+}
