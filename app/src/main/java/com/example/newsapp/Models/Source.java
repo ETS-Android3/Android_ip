2677f7c6421802090c0408534cd8c241124de20e
Mem Rop <judyrop97@gmail.com>
2021-10-22 15:26:37 +0300

    added changes to the files

--- app/src/main/java/com/example/newsapp/Models/Source.java
+++ app/src/main/java/com/example/newsapp/Models/Source.java
@@ -17,13 +17,13 @@ public class Source {
 
     /**
      * No args constructor for use in serialization
-     *
+     * 
      */
     public Source() {
     }
 
     /**
-     *
+     * 
      * @param name
      * @param id
      */
@@ -47,46 +47,6 @@ public class Source {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(Source.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
-        sb.append("id");
-        sb.append('=');
-        sb.append(((this.id == null)?"<null>":this.id));
-        sb.append(',');
-        sb.append("name");
-        sb.append('=');
-        sb.append(((this.name == null)?"<null>":this.name));
-        sb.append(',');
-        if (sb.charAt((sb.length()- 1)) == ',') {
-            sb.setCharAt((sb.length()- 1), ']');
-        } else {
-            sb.append(']');
-        }
-        return sb.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        int result = 1;
-        result = ((result* 31)+((this.name == null)? 0 :this.name.hashCode()));
-        result = ((result* 31)+((this.id == null)? 0 :this.id.hashCode()));
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-        if ((other instanceof Source) == false) {
-            return false;
-        }
-        Source rhs = ((Source) other);
-        return (((this.name == rhs.name)||((this.name!= null)&&this.name.equals(rhs.name)))&&((this.id == rhs.id)||((this.id!= null)&&this.id.equals(rhs.id))));
     }
 
 }
