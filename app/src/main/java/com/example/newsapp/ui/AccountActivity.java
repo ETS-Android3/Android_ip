a3f1634dcc344162ea4e08b378c1b92ea89b24ed
Mem Rop <judyrop97@gmail.com>
2021-10-25 09:52:53 +0300

    Created loginActivity and create account activity

--- app/src/main/java/com/example/newsapp/ui/AccountActivity.java
+++ app/src/main/java/com/example/newsapp/ui/AccountActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RestaurantDetailActivity extends AppCompatActivity {
+import com.example.newsapp.R;
+
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant_detail);
+        setContentView(R.layout.activity_account);
     }
 }
